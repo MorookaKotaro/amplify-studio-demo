c033b9d513abaf7a3fa4fd550957eb52673b778d
MorookaKotaro <morookakotaro@team-lab.com>
2021-12-15 04:27:18 +0900

    Updates

---
+++ precache-manifest.4347e279fed50075c3b82e03bb16a614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff7d9a1e7bc6da71c9d4",
+    "url": "/amplify-studio-demo/static/css/main.b794d9df.chunk.css"
+  },
+  {
+    "revision": "ff7d9a1e7bc6da71c9d4",
+    "url": "/amplify-studio-demo/static/js/main.2b96e5bf.chunk.js"
+  },
+  {
+    "revision": "d7a1a7316a3d985f8b26",
+    "url": "/amplify-studio-demo/static/js/runtime~main.4cd8ba46.js"
+  },
+  {
+    "revision": "9271576b804a600a0676",
+    "url": "/amplify-studio-demo/static/css/2.491c859a.chunk.css"
+  },
+  {
+    "revision": "9271576b804a600a0676",
+    "url": "/amplify-studio-demo/static/js/2.0ab7c0ab.chunk.js"
+  },
+  {
+    "revision": "f6531af4365ce117081a",
+    "url": "/amplify-studio-demo/static/js/3.2b83aebb.chunk.js"
+  },
+  {
+    "revision": "7c7afe3f1f04e4a05c13523748013fdd",
+    "url": "/amplify-studio-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7c4811b2c5d14224351df4e09454154.js"
+  "/amplify-studio-demo/precache-manifest.4347e279fed50075c3b82e03bb16a614.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/amplify-studio-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
